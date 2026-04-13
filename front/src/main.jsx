93302702095573ae235fdbfcb69d023b27cad5bd
JohnOnias <john.emnauel.onias07@aluno.ifce.edu.br>
2026-04-13 16:48:14 -0300

    init login

--- front/src/main.jsx
+++ front/src/main.jsx
@@ -3,8 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './styles/index.css'
 import Login from './pages/login/Login'; 
 
+
 createRoot(document.getElementById('root')).render(
+
   <StrictMode>
     <Login />
   </StrictMode>,
+  
 )
--- front/src/pages/login/Login.jsx
+++ front/src/pages/login/Login.jsx
@@ -1,13 +1,34 @@
 import React from 'react'
-import Navbar from '../../components/Navbar/Navbar'; 
-import Footer from '../../components/Footer/Footer';
+import Locador from '../../assets/imgs/locador.png';
+import Locatario from '../../assets/imgs/Locatario.png';
+
 export default function Login() {
   return (
     <>
-        <Navbar></Navbar>
-    <div>login</div>
 
-    <Footer/>
+
+  <div className='h-full w-full '> 
+
+
+
+
+
+      <div className='bg-white flex h-[75vh] w-[35vw] rounded-[25px]'>
+          
+          
+          <img src={Locador} alt="locador img"  className='w=[35rem] h-[278px] m-[10px]'/>
+          
+          <img src={Locatario} alt="loctario img" className='w=[214px] h-[278px]  m-[10px]' />
+
+          
+        </div>
+
+
+
+
+  </div>
+   
+    
   </>
   )
 }
